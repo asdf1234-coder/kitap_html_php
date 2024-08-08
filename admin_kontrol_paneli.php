81dc66e601d562023a27212510607a97a273df61
asdf1234-coder <penspinning222@gmail.com>
2024-08-08 17:43:39 +0300

    Update admin_kontrol_paneli.php

--- admin_kontrol_paneli.php
+++ admin_kontrol_paneli.php
@@ -8,6 +8,16 @@
     echo "parola boş bırakılamaz tekrar deneyiniz";
     exit;
 }?>
+<?php $get_yayin = $_GET["yayinevi"] ?>
+<?php if (empty($get_yayin)) $get_yayin_kontrol = True ?>
+<?php $get_tur = $_GET["tur"]?>
+<?php if (empty($get_tur)) $get_tur_kontrol = True ?>
+<?php $get_yazar = $_GET["yazar"] ?>
+<?php if (empty($get_yazar)) $get_yazar_kontrol = True ?>
+<?php $get_min = $_GET["min"]?>
+<?php if (empty($get_min)) $get_min_kontrol = True ?>
+<?php $get_max = $_GET["max"]?>
+<?php if (empty($get_max)) $get_max_kontrol = True ?>
 <?php
     $host = "localhost";
     $username = "root";
@@ -18,6 +28,9 @@
     $quary = "SELECT * from admin";
     $result = mysqli_query($baglanti, $quary);
     $profiller = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $quary_kitap = "SELECT * from kitaplar";
+    $result_kitap = mysqli_query($baglanti, $quary_kitap);
+    $tum_kitaplar = mysqli_fetch_all($result_kitap, MYSQLI_ASSOC);
     mysqli_close($baglanti);
 ?>
 <?php foreach($profiller as $profil) {
@@ -30,7 +43,6 @@
         $giris = false;
     }
     if (($parola == true) && ($isim == true)) {
-        echo "giriş başarılı";
         $giris = true;
     }
 }?>
@@ -44,9 +56,73 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Kontrol Paneli</title>
+        <link rel="stylesheet" type="text/css" href="style9.css" />
     </head>
     <body>
-        <div>burası kontrol paneli</div>
+        <div class="hheader">
+            <div class="baslik bosluk">Kitap_HTML</div>
+            <a href="index.php"><div class="anasayfa bosluk">Anasayfa</div></a>
+            <div class="kitaplar bosluk">Tüm Kitaplar</div>
+            <a href="admin_login.php"><button class="buton">ADMIN LOGIN</button></a>
+        </div>
+        <div class="butonlar">
+            <button class="eklesil ekle">EKLE</button>
+            <button class="eklesil sil" onclick="showElements()">SİL</button>
+            <button class="eklesil butonlar_gizli" onclick="hiddenElements()">BUTONLARI GİZLE</button>
+        </div>
+        <div class="kitap_container">
+            <div class="form">
+                <form action="admin_kontrol_paneli.php" method="get">
+                    <div class ="container_filtreleme_kontrol">
+                        <input type="text" name="adminadi" placeholder="kullanıcı ismini giriniz">
+                        <input type="password" name="adminparola" placeholder="parlolanızı giriniz">
+                        <input type="text" name="yayinevi" placeholder="yayınevi giriniz">
+                        <input type="text" name="tur" placeholder="tür giriniz">
+                        <input type="text" name="yazar" placeholder="yazar giriniz">
+                        <input type="number" id="price" name="min" min="0" step="0.01" placeholder="min değer giriniz">
+                        <input type="number" id="price" name="max" min="0" step="0.01" placeholder="max değer giriniz">
+                        <button type="submit" class="button">GÖNDER</button>
+                    </div>
+                </form>
+            </div>
+            <div class="tum_kitaplar">
+                <?php foreach($tum_kitaplar as $kitap): ?>
+                    <?php if (empty($get_yayin)) {
+                        $get_yayin = $kitap["yayinevi"];
+                    }?>
+                    <?php if (empty($get_tur)) {
+                        $get_tur = $kitap["tur"];
+                    }?>
+                    <?php if (empty($get_yazar)) {
+                        $get_yazar = $kitap["yazar"];
+                    }?>
+                    <?php if (empty($get_min)) {
+                        $get_min = 0;
+                    }?>
+                    <?php if (empty($get_max)) {
+                        $get_max = 999999999999999;
+                    }?>
+                    <?php if (($get_tur == $kitap["tur"]) && ($get_yayin == $kitap["yayinevi"]) && ($get_yazar == $kitap["yazar"]) && ($get_min < $kitap["ucret"] && $get_max > $kitap["ucret"])): ?>
+                        <a href = "kitap_detay.php?id=<?php echo $kitap["id"]?>">
+                            <div class="kitap">
+                                <div class="ust"><img src="img/<?php echo $kitap["resim"]?>" height="100%" width="100%"></div>
+                                <div class="yazar_isim">
+                                    <div class="alt fiyat"><?php echo $kitap["ucret"]?> TL</div>
+                                    <div class="alt"><?php echo $kitap["isim"] ?></div>
+                                    <div class="buton_kitap"><button class="button_div alt">SİL</button></div>
+                                </div>
+                            </div>
+                        </a>
+                    <?php endif; ?>
+                    <?php if ($get_yayin_kontrol == True) $get_yayin = null ?>
+                    <?php if ($get_tur_kontrol == True) $get_tur = null ?>
+                    <?php if ($get_yazar_kontrol == True) $get_yazar = null ?>
+                    <?php if ($get_min_kontrol == True) $get_min = null ?>
+                    <?php if ($get_max_kontrol == True) $get_max = null ?>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <script src="script2.js"></script>
     </body>
     </html>
 <?php endif; ?>
