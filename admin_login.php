15b291176ea2c694e8104fc3abbb41f5a1c640ea
asdf1234-coder <penspinning222@gmail.com>
2024-08-12 08:59:02 +0300

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin login</title>
-    <link rel="stylesheet" type="text/css" href="style9.css" />
+    <link rel="stylesheet" type="text/css" href="style12.css" />
 </head>
 <body>
     <div class="adminform">
         <form action="admin_kontrol_paneli.php" method="post">
-            <div class ="container_filtreleme">
+            <div class ="container_filtreleme_login">
                 <input type="text" name="adminadi" placeholder="kullanıcı ismini giriniz">
                 <input type="password" name="adminparola" placeholder="parlolanızı giriniz">
-                <button type="submit" class="button">GÖNDER</button>
+                <button type="submit" class="button_login">GÖNDER</button>
             </div>
         </form>
     </div>
